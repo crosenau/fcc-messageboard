fbb24ec5c3edd8a89a94c10f2c8ea0cabf5cb05c
crosenau <rosenauc74@hotmail.com>
2019-02-05 00:15:56 -0800

    set up helmetjs, mongoDB

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+require('dotenv').config();
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const helmet      = require('helmet');
 
-var app = express();
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+
+const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -17,54 +21,76 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet({
+  frameguard: { action: 'sameorigin' },
+  dnsPrefetchControl: { allow: false },
+  referrerPolicy: { policy: 'same-origin' }
+}));
 
-//Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);
-
-//Sample Front-end
-
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use((req, res, next) => {
+  console.log('\n');
+  console.log(`New ${req.method} request to ${req.path} from ${req.ip}`);
+  console.log('x-forwarded-for: ', req.headers['x-forwarded-for']);
+  next();
 });
 
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+const client = new MongoClient(process.env.DATABASE_URI, { useNewUrlParser: true });
+
+client.connect()
+  .then(() => {
+    console.log('Successfully connected to database');
+
+    const db = client.db('fcc');
+
+    //For FCC testing purposes
+    fccTestingRoutes(app);
+    apiRoutes(app, db);
+
+    //Sample front-end
+    app.route('/b/:board/')
+    .get((req, res) => {
+      res.sendFile(process.cwd() + '/views/board.html');
+    });
+
+    app.route('/b/:board/:threadid')
+    .get((req, res) => {
+      res.sendFile(process.cwd() + '/views/thread.html');
+    });
+
+    app.route('/')
+    .get((req, res) => {
+      res.sendFile(process.cwd() + '/views/index.html');
+    });
+      
+    //404 Not Found Middleware
+    app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+    });
+
+    //Start our server and tests!
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+      if(process.env.NODE_ENV==='test') {
+        console.log('Running Tests...');
+        setTimeout(() => {
+          try {
+            runner.run();
+          } catch(e) {
+            const error = e;
+              console.log('Tests are not valid:');
+              console.log(error);
+          }
+        }, 1500);
       }
-    }, 1500);
-  }
-});
+    });
+  })
+  .catch(err => {
+    console.log('Error connecting to database');
+    console.log(err);
+  });
 
 module.exports = app; //for testing
